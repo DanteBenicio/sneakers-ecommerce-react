efd3b1a6155e73c2b2f1ca065461a2a2f68a55af
DanteBenicio <dantecosta79@gmail.com>
2022-02-16 17:46:47 -0300

    create theme colors

---
+++ src/theme/index.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    primary: 'hsl(26, 100%, 55%)',
+    secondary: 'hsl(25, 100%, 94%)',
+    veryVarkBlue: 'hsl(220, 13%, 13%)',
+    darkGrayishBlue: 'hsl(219, 9%, 45%)',
+    grayishBlue: 'hsl(220, 14%, 75%)',
+    lightGrayishBlue: 'hsl(223, 64%, 98%)',
+    white: 'hsl(0, 0%, 100%)',
+    black: 'hsl(0, 0%, 0%)',
+  },
+};
