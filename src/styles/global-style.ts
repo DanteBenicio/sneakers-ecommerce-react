ceb2e2fa7a9ce0bdedd9437dd7c96add2b88f13b
DanteBenicio <dantecosta79@gmail.com>
2022-02-15 22:55:50 -0300

    global styles

---
+++ src/styles/global-style.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Kumbh Sans', 'Open Sans', 'Arial', 'Helvetica', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html, body, #root {
+    max-width: 100vw;
+    min-height: 100vh;
+  }
+`;
+
+export default GlobalStyle;
