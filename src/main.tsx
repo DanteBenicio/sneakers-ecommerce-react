297c0c2848e61237bc0b2bcd637fb0a25b08f1b1
DanteBenicio <dantecosta79@gmail.com>
2022-02-15 23:05:41 -0300

    added theme and theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
 import Home from './pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
