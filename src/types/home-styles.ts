195a4a0d95dc55a73f0000dbde9fb4d5c53b5e0e
DanteBenicio <dantecosta79@gmail.com>
2022-02-18 21:49:18 -0300

    create product type

--- src/types/home-styles.ts
+++ src/types/home-styles.ts
@@ -1,3 +1,11 @@
 export type ImageItemProps = {
   imgSelected: boolean
 }
+
+export type ProductType = {
+  imgSrc: string
+  id: number
+  sneakerName: string
+  value: number
+  pairsQuantity: number
+}
